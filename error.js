50b657477308036ddfe56e984dcf2aef2a496fb4
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-03 22:46:53 +0100

    Module with html error page added

---
+++ error.js
@@ -0,0 +1,15 @@
+const errorHtml = () => `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Error</title>
+  </head>
+  <body>
+    <h2>Error!</h2>
+    <p>One of the parameters is not a number</p>
+  </body>
+</html>
+`;
+module.exports = errorHtml;
